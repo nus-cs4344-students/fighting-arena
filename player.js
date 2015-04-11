1885df64180fbc8f0eef47a5643aeaf591e9a005
chenzeyu <zeyufly@gmail.com>
2015-04-11 20:11:41 +0800

    add player.js

---
+++ player.js
@@ -0,0 +1,23 @@
+function Player(sid, pid, yPos) {
+    // Public variables
+    this.sid;   // Socket id. Used to uniquely identify players via 
+                // the socket they are connected from
+    this.pid;   // Player id. In this case, 1 or 2 
+    this.fighter;// player's paddle object 
+    this.lastUpdated; // timestamp of last paddle update
+
+    // Constructor
+    this.sid = sid;
+    this.pid = pid;
+    this.lastUpdated = new Date().getTime();
+
+    /*
+     * priviledge method: getDelay
+     *
+     * Return the current delay associated with this player.
+     * The delay has a random 20% fluctuation.
+     */
+}
+
+// For node.js require
+global.Player = Player;
---
+++ setting.js
@@ -0,0 +1,16 @@
+/*=====================================================
+  Declared as literal object (All variables are static)	  
+  =====================================================*/
+var Setting = {
+	HEIGHT : 768,				// height of fighter game window
+	WIDTH : 1280,				// width of fighter game window
+	PORT : 3333,				// port of fighter game
+	FRAME_RATE : 25,			// frame rate of fighter game
+	SERVER_NAME : "localhost"	// server name of fighter game
+	//SERVER_NAME : "172.28.179.28"	// server name of fighter game
+}
+
+// For node.js require
+global.Setting = Setting;
+
+// vim:ts=4
