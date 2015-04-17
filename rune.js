ddc855454db5c59999aa93d4bb9016e99b6da4f5
chenzeyu <zeyufly@gmail.com>
2015-04-18 04:09:44 +0800

    add rune

--- rune.js
+++ rune.js
@@ -1,10 +1,15 @@
 /**
  * Created by zchen on 17/4/15.
  */
-function Rune (type) {
-    this.type = type;
+function Rune(type, xpos, ypos) {
     this.collected_at;
+    this.type = type;
+    this.x = xpos;
+    this.y = ypos;
     this.duration = 10000;
+    this.name = undefined;
 }
 
+Rune.WIDTH = 32;
+Rune.HEIGHT = 32;
 global.Rune = Rune;
